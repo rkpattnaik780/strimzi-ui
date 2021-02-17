bdf74b8d96abab5864fb3e840bf152e58372340d
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2021-02-17 18:55:25 +0530

    Fix: sidebar logic for active route
    sidebar was failing to keep track of the active route

--- client/Elements/Components/AppLayout/Sidebar.patternfly.tsx
+++ client/Elements/Components/AppLayout/Sidebar.patternfly.tsx
@@ -4,31 +4,36 @@
  */
 
 import { Nav, NavItem, NavList, PageSidebar } from '@patternfly/react-core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const SideBar: React.FC = () => {
 
-  const getActiveRoute = (index: number): boolean => {
-    const routes = [
-      "/",
-      "/topics"
-    ];
+  const [activeItem, setActiveItem] = useState<number>(0);
 
-    if(routes[index] === window.location.hash.split("#")[1]) {
-      return true;
-    }
-    return false;
+  useEffect(() => {
+    setActiveItem(
+      routeToId[window.location.hash.split("#")[1] || "/"]
+    )
+  }, []);
+
+  const routeToId = {
+    "/": 0,
+    "/topics": 1
+  };
+
+  const onSelect = (result) => {
+    setActiveItem(result.itemId)
   }
 
   const PageNav = (
-    <Nav aria-label='side-navigation-bar'>
+    <Nav aria-label='side-navigation-bar' onSelect={onSelect}>
       <NavList>
         <NavItem
           itemId={0}
-          isActive={getActiveRoute(0)}
-          to='/'> Home
+          isActive={activeItem === 0}
+          to='/#/'> Home
         </NavItem>
-        <NavItem itemId={1} isActive={getActiveRoute(1)} to='/#/topics'> Topics </NavItem>
+        <NavItem itemId={1} isActive={activeItem === 1} to='/#/topics'> Topics </NavItem>
         <NavItem itemId={2} isActive={false}> Consumer groups </NavItem>
         <NavItem itemId={3} isActive={false}> Cluster configuration </NavItem>
       </NavList>
